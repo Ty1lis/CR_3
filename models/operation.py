c775b96a8ed03f66c501b6dff345fe0adb47c7bb
Ilya Tulisov <tulisov2001@mail.ru>
2023-12-26 16:12:02 +0300

    Добавить данные класса

--- models/operation.py
+++ models/operation.py
@@ -1,2 +1,18 @@
 class Operation:
-    pass
+    def __init__(
+            self,
+            pk,
+            date,
+            state,
+            operation_amount,
+            description,
+            from_,
+            to,
+   ):
+        self.pk = pk
+        self.date = date
+        self.state = state
+        self.operation_amount = operation_amount
+        self.description = description
+        self.from_ = from_
+        self.to = to
