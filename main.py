05839390a1cf444870f99eead729e5aa2a23e08d
Ilya Tulisov <tulisov2001@mail.ru>
2023-12-27 10:29:20 +0300

    init functions

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    pass
+from models.operation import read_json_file
+from models.views import show_last_five_operations
+from settings import OPERATION_PATH
 
 
-if __name__ == '__main__':
+def main() -> None:
+    data = read_json_file(OPERATION_PATH)
+
+    transactions = show_last_five_operations(data)
+
+    for transaction in transactions:
+        print(
+            f"{transaction['date']} {transaction['description']}\n"
+            f"{transaction.get('from', 'нет информации счет/карта')} -> {transaction['to']}\n"
+            f"{transaction['operationAmount']['amount']} {transaction['operationAmount']['currency']['name']}\n"
+        )
+
+
+if __name__ == "__main__":
     main()
